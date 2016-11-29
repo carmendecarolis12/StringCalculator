ebaf58630caa4b3f21073e149296650b998ee438
carmendecarolis12 <c.decarolis1@studenti.uniba.it>
2016-11-29 16:44:31 +0100

    Mancano gli ultimi due punti

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,9 +1,36 @@
 package it.uniba.tdd;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		// not yet implemented
-		return 0;
+	
+	public static int add(String numbersStr) throws StringCalculatorException  {
+		int sum = 0; 
+		String delimiter = ",|\\n";
+		
+		if (numbersStr == ""){
+			return 0; // in caso di stringa vuota
+		}
+		
+		if (numbersStr.charAt(0) == '/' && numbersStr.charAt(1) == '/'){ //separatori speciali
+			String[] sepSpe = numbersStr.split("\n|//");
+			delimiter = sepSpe[1]; 
+			String[] sepNewLine = numbersStr.split("\n");
+			numbersStr = sepNewLine[1];
+			
+		}
+		String[] sep = numbersStr.split(delimiter);
+		
+		for (String string : sep) { //caso base
+			/*if (Integer.parseInt(string) < 0 && Integer.parseInt(string) > 1000){
+				throw new StringCalculatorException(); */
+			
+			if (string.isEmpty()){ // ,\n oppure \n,
+				throw new StringCalculatorException();
+			}
+			if(Integer.parseInt(string)>= 0 && Integer.parseInt(string)<1000){	
+			 sum += Integer.parseInt(string);
+			}
+		}
+			
+		return sum;
 	}
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -4,11 +4,91 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import it.uniba.tdd.StringCalculator;
+import it.uniba.tdd.StringCalculatorException;
+
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStringIsEmpy() throws StringCalculatorException {
+		//arrange
+		String numberString = "";
+		//act
+		int calculator = StringCalculator.add(numberString);
+		//assert
+		assertEquals(0, calculator);
 	}
 
+	@Test
+	public void testStringIsOneNumber() throws StringCalculatorException {
+		//arrange
+		String numberString = "20";
+		//act
+		int calculator = StringCalculator.add(numberString);
+		//assert
+		assertEquals(20, calculator);
+	}
+	@Test
+	public void testStringIsTwoNumbers() throws StringCalculatorException {
+		//arrange
+		String numberString = "20,50";
+		//act
+		int calculator = StringCalculator.add(numberString);
+		//assert
+		assertEquals(70, calculator);
+	}
+	@Test
+	public void testStringIsFiveNumbers() throws StringCalculatorException {
+		//arrange
+		String numberString = "20,50,3,2,90";
+		//act
+		int calculator = StringCalculator.add(numberString);
+		//assert
+		assertEquals(165, calculator);
+	}
+	@Test
+	public void testStringIsNewLine() throws StringCalculatorException{
+		//arrange
+		String numberString = "1\n2,3";
+		//act
+		int calculator = StringCalculator.add(numberString);
+		//assert
+		assertEquals(6, calculator);
+	}
+	@Test(expected = StringCalculatorException.class)
+	public void testStringIsNewLineWRONG() throws StringCalculatorException   {
+		//arrange
+		String numberString = "1,\n"; //non accettato
+		//act
+		int calculator = StringCalculator.add(numberString);
+		//assert
+	}
+	@Test
+	public void testStringIsNewLineWRONG2() throws StringCalculatorException   {
+		//arrange
+		String numberString = "2,1003"; //non accettato
+		//act
+		int calculator = StringCalculator.add(numberString);
+		//assert
+		assertEquals(2, calculator);
+	}
+		 
+	@Test
+	public void testStringIsNewDelimiter() throws StringCalculatorException{
+		//arrange
+		String numberString = "//;\n1;2";
+		//act
+		int calculator = StringCalculator.add(numberString);
+		//assert
+		assertEquals(3, calculator);
+	}
+	@Test
+	public void testStringIsNewDelimiter2() throws StringCalculatorException{
+		//arrange
+		String numberString = "//-\n1-2-20-30";
+		//act
+		int calculator = StringCalculator.add(numberString);
+		//assert
+		assertEquals(53, calculator);
+	}
 }
